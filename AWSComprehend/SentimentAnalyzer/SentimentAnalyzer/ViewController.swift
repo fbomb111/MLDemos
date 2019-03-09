0cbcce4b90bf13de033be592ce718b95d192614c
Frankie Cleary <fcleary@captechconsulting.com>
2019-03-09 16:30:49 -0500

    Add interface for analyzing text

--- AWSComprehend/SentimentAnalyzer/SentimentAnalyzer/ViewController.swift
+++ AWSComprehend/SentimentAnalyzer/SentimentAnalyzer/ViewController.swift
@@ -11,6 +11,9 @@ import AWSComprehend
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
     enum Sentiment: String {
         case positive
         case negative
@@ -19,39 +22,39 @@ class ViewController: UIViewController {
         
         init?(index: Int) {
             switch index {
-            case 0:
-                self = .positive
             case 1:
-                self = .negative
+                self = .positive
             case 2:
-                self = .neutral
+                self = .negative
             case 3:
+                self = .neutral
+            case 4:
                 self = .mixed
             default:
                 return nil
             }
         }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    @IBAction func analyze() {
         let comprehend = AWSComprehend.default()
         
         do {
             // docs: https://docs.aws.amazon.com/comprehend/latest/dg/API_DetectSentiment.html
             let request = try AWSComprehendDetectSentimentRequest(dictionary: [:], error: ())
             request.languageCode = .en
-            request.text = "Today it's raining in Seattle"
-        
+            request.text = self.textView.text
+            
             comprehend.detectSentiment(request).continueWith { task in
                 
-                if let error = task.error {
-                    print("Error occurred: \(error)")
-                }
-                
-                if let result = task.result?.sentiment, let sentiment = Sentiment(index: result.rawValue) {
-                    print(sentiment)
+                DispatchQueue.main.async {
+                    if let error = task.error {
+                        self.handleError(error: error)
+                    }
+                    
+                    if let result = task.result?.sentiment, let sentiment = Sentiment(index: result.rawValue) {
+                        self.sentimentLabel.text = "Analysis: \(sentiment.rawValue.capitalized)"
+                    }
                 }
                 
                 return nil
@@ -60,5 +63,18 @@ class ViewController: UIViewController {
         } catch {
             print("error \(error.localizedDescription)")
         }
+    }
+    
+    private func handleError(error: Error) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.sentimentLabel.text = "Analysis: ???"
     }
 }
